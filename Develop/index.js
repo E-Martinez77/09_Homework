2dbc25ea008c950b13bd6bc7e85dbde8b9f48818
E-Martinez77 <emartizzle2185@me.com>
2021-02-10 18:10:21 -0700

    Updated functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,67 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "username",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "projectname",
+    message: "What is the name of the project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project",
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "What kind of license should your project have?",
+  },
+  {
+    type: "input",
+    name: "dependencies",
+    message: "What command should be run to install dependencies?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What command should be run to run tests?",
+  },
+  {
+    type: "input",
+    name: "about",
+    message: "What does the user need to know about using the repo?",
+  },
+  {
+    type: "input",
+    name: "contribute",
+    message: "What does the user need to know about contributing to the repo?",
+  },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
 function init() {
-
+  inquirer.prompt(questions).then((answers) => {
+    writeToFile("Readme.md", generateMarkdown({ ...answers }));
+  });
 }
 
 // function call to initialize program
